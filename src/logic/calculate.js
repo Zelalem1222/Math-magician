d9416135fce5e809fa2255b3def3093834549039
Zelalem1222 <zelalemfissha394@gmail.com>
2022-07-07 03:46:10 -0400

    Update calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,13 +1,11 @@
-/* eslint-disable */
-
 import operate from './operate';
 
 function isNumber(item) {
   return !!item.match(/[0-9]+/);
 }
 
-export default function calculate(obj, buttonName) {
-  if (buttonName === 'AC') {
+export default function calculate(obj, pressedButton) {
+  if (pressedButton === 'AC') {
     return {
       total: null,
       next: null,
@@ -15,31 +13,31 @@ export default function calculate(obj, buttonName) {
     };
   }
 
-  if (isNumber(buttonName)) {
-    if (buttonName === '0' && obj.next === '0') {
+  if (isNumber(pressedButton)) {
+    if (pressedButton === '0' && obj.next === '0') {
       return {};
     }
 
     if (obj.operation) {
       if (obj.next && obj.next !== '0') {
-        return { ...obj, next: obj.next + buttonName };
+        return { ...obj, next: obj.next + pressedButton };
       }
-      return { ...obj, next: buttonName };
+      return { ...obj, next: pressedButton };
     }
 
     if (obj.next && obj.next !== '0') {
       return {
-        next: obj.next + buttonName,
+        next: obj.next + pressedButton,
         total: null,
       };
     }
     return {
-      next: buttonName,
+      next: pressedButton,
       total: null,
     };
   }
 
-  if (buttonName === '.') {
+  if (pressedButton === '.') {
     if (obj.next) {
       if (obj.next.includes('.')) {
         return { ...obj };
@@ -58,7 +56,7 @@ export default function calculate(obj, buttonName) {
     return { ...obj, next: '0.' };
   }
 
-  if (buttonName === '=') {
+  if (pressedButton === '=') {
     if (obj.next && obj.operation) {
       return {
         total: operate(obj.total, obj.next, obj.operation),
@@ -66,11 +64,11 @@ export default function calculate(obj, buttonName) {
         operation: null,
       };
     }
-   
+
     return {};
   }
 
-  if (buttonName === '+/-') {
+  if (pressedButton === '+/-') {
     if (obj.next) {
       return { ...obj, next: (-1 * parseFloat(obj.next)).toString() };
     }
@@ -80,38 +78,33 @@ export default function calculate(obj, buttonName) {
     return {};
   }
 
-  
   if (!obj.next && obj.total && !obj.operation) {
-    return { ...obj, operation: buttonName };
+    return { ...obj, operation: pressedButton };
   }
 
-  
   if (obj.operation) {
     if (obj.total && !obj.next) {
-      return { ...obj, operation: buttonName };
+      return { ...obj, operation: pressedButton };
     }
 
     if (!obj.total) {
-      return { total: 0, operation: buttonName };
+      return { total: 0, operation: pressedButton };
     }
 
     return {
       total: operate(obj.total, obj.next, obj.operation),
       next: null,
-      operation: buttonName,
+      operation: pressedButton,
     };
   }
 
-  
   if (!obj.next) {
-    return { operation: buttonName };
+    return { operation: pressedButton };
   }
 
-  
   return {
     total: obj.next,
     next: null,
-    operation: buttonName,
+    operation: pressedButton,
   };
 }
-
