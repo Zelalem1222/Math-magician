045c7cb6839fe0501f861265838811089c23c911
Zelalem1222 <zelalemfissha394@gmail.com>
2022-07-07 12:42:24 -0400

    Modify calculator.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -5,64 +5,62 @@ import calculate from '../logic/calculate';
 
 
 function Calculator(){
+
+
   const [state, setState] = useState({
     next: null,
-    total: null,
+    total: '',
     operation: null,
   });
  
-
-  const calculateFunction = (e) => {
-    setState((state) => {
-      calculate({
-        total: state.total,
-        next: state.next,
-        operation: state.operation
-      },
-      e.target.value,
-      console.log(e.target.value)
-      )
-    })
+ 
+  const handleClick = (e)=> {
+    const pressedButton = e.target.innerText;
+    const newState = calculate(state, pressedButton);
+    setState(newState);
   }
+ 
 
 
-  const { next,total,operation } = setState;
+  const { next , total, operation } = state;
 return (
 
     <div className="container">
+     
+
       <div className="display">
         <span>{next}</span>
         <span>{operation}</span>
         <span>{total}</span>
       </div>
       <div>
-        <button value={'AC'} type="button" onClick={calculateFunction}>AC</button>
-        <button value={''} type="button" onClick={calculateFunction}>+/-</button>
-        <button value="%" type="button" onClick={calculateFunction}>%</button>
-        <button value="/" className="orange" onClick={calculateFunction} type="button"> &#247;</button>
+        <button value={'AC'} type="button" onClick={handleClick}>AC</button>
+        <button value={'+/-'} type="button" onClick={handleClick}>+/-</button>
+        <button value={"%"} type="button" onClick={handleClick}>%</button>
+        <button value={"/"} className="orange" onClick={handleClick} type="button"> &#247;</button>
       </div>
       <div>
-        <button value={7} type="button" onClick={calculateFunction}>7</button>
-        <button value={8} type="button" onClick={calculateFunction}>8</button>
-        <button value={9} type="button" onClick={calculateFunction}>9</button>
-        <button value="*" className="orange" onClick={calculateFunction} type="button">x</button>
+        <button value={'7'} type="button" onClick={handleClick}>7</button>
+        <button value={'8'} type="button" onClick={handleClick}>8</button>
+        <button value={'9'} type="button" onClick={handleClick}>9</button>
+        <button value={"*"} className="orange" onClick={handleClick} type="button">x</button>
       </div>
       <div>
-        <button value={4} type="button" onClick={calculateFunction}>4</button>
-        <button value={5} type="button" onClick={calculateFunction}>5</button>
-        <button value={6} type="button" onClick={calculateFunction}>6</button>
-        <button value="-" className="orange" onClick={calculateFunction} type="button">-</button>
+        <button value={'4'} type="button" onClick={handleClick}>4</button>
+        <button value={'5'} type="button" onClick={handleClick}>5</button>
+        <button value={'6'} type="button" onClick={handleClick}>6</button>
+        <button value={"-"}className="orange" onClick={handleClick} type="button">-</button>
       </div>
       <div>
-        <button value={1} type="button" onClick={calculateFunction}>1</button>
-        <button value={2} type="button" onClick={calculateFunction}>2</button>
-        <button value={3} type="button" onClick={calculateFunction}>3</button>
-        <button value="+" className="orange" onClick={calculateFunction} type="button">+</button>
+        <button value={'1'} type="button" onClick={handleClick}>1</button>
+        <button value={'2'} type="button" onClick={handleClick}>2</button>
+        <button value={'3'} type="button" onClick={handleClick}>3</button>
+        <button value={"+"} className="orange" onClick={handleClick} type="button">+</button>
       </div>
       <div>
-        <button value={0} onClick={calculateFunction} className="wide-button" type="button">0</button>
-        <button value="." type="button" onClick={calculateFunction}>.</button>
-        <button onClick={calculateFunction} className="orange" type="button">=</button>
+        <button value={'0'} onClick={handleClick} className="wide-button" type="button">0</button>
+        <button value="." type="button" onClick={handleClick}>.</button>
+        <button value={'='} onClick={handleClick} className="orange" type="button">=</button>
       </div>
     </div>
   );
