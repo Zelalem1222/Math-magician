d4c38cf226c3f821d5b9f45bbba3f1e192c4974d
Zelalem1222 <zelalemfissha394@gmail.com>
2022-07-13 05:54:13 -0400

    Modify the app

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -18,40 +18,40 @@ function Calculator() {
   return (
 
     <div className="container">
-      <h1>Simple Calculator</h1>
+      <h1>Let&apos;s do some math!</h1>
       <div className="display">
         <span>{next}</span>
         <span>{operation}</span>
         <span>{total}</span>
       </div>
       <div>
-        <button value="AC" type="button" onClick={handleClick}>AC</button>
-        <button value="+/-" type="button" onClick={handleClick}>+/-</button>
-        <button value="%" type="button" onClick={handleClick}>%</button>
-        <button value="/" className="orange" onClick={handleClick} type="button"> &#247;</button>
+        <button type="button" onClick={handleClick}>AC</button>
+        <button type="button" onClick={handleClick}>+/-</button>
+        <button type="button" onClick={handleClick}>%</button>
+        <button className="orange" onClick={handleClick} type="button"> &#247;</button>
       </div>
       <div>
-        <button value="7" type="button" onClick={handleClick}>7</button>
-        <button value="8" type="button" onClick={handleClick}>8</button>
-        <button value="9" type="button" onClick={handleClick}>9</button>
-        <button value="*" className="orange" onClick={handleClick} type="button">x</button>
+        <button type="button" onClick={handleClick}>7</button>
+        <button type="button" onClick={handleClick}>8</button>
+        <button type="button" onClick={handleClick}>9</button>
+        <button className="orange" onClick={handleClick} type="button">x</button>
       </div>
       <div>
-        <button value="4" type="button" onClick={handleClick}>4</button>
-        <button value="5" type="button" onClick={handleClick}>5</button>
-        <button value="6" type="button" onClick={handleClick}>6</button>
-        <button value="-" className="orange" onClick={handleClick} type="button">-</button>
+        <button type="button" onClick={handleClick}>4</button>
+        <button type="button" onClick={handleClick}>5</button>
+        <button type="button" onClick={handleClick}>6</button>
+        <button className="orange" onClick={handleClick} type="button">-</button>
       </div>
       <div>
-        <button value="1" type="button" onClick={handleClick}>1</button>
-        <button value="2" type="button" onClick={handleClick}>2</button>
-        <button value="3" type="button" onClick={handleClick}>3</button>
-        <button value="+" className="orange" onClick={handleClick} type="button">+</button>
+        <button type="button" onClick={handleClick}>1</button>
+        <button type="button" onClick={handleClick}>2</button>
+        <button type="button" onClick={handleClick}>3</button>
+        <button className="orange" onClick={handleClick} type="button">+</button>
       </div>
       <div>
-        <button value="0" onClick={handleClick} className="wide-button" type="button">0</button>
-        <button value="." type="button" onClick={handleClick}>.</button>
-        <button value="=" onClick={handleClick} className="orange" type="button">=</button>
+        <button onClick={handleClick} className="wide-button" type="button">0</button>
+        <button type="button" onClick={handleClick}>.</button>
+        <button onClick={handleClick} className="orange" type="button">=</button>
       </div>
     </div>
   );
